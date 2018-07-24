b4b4178c38f250580d5c719f931dbf824997d075
leeuwm1 <mitchel.de.leeuw@hva.nl>
2018-07-24 13:20:23 +0200

    gerecht model added

---
+++ app/Http/Controllers/GerechtController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Homeserver\Http\Controllers;
+
+use Homeserver\Gerecht;
+use Illuminate\Http\Request;
+
+class GerechtController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Homeserver\Gerecht  $gerecht
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gerecht $gerecht)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Homeserver\Gerecht  $gerecht
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gerecht $gerecht)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Homeserver\Gerecht  $gerecht
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gerecht $gerecht)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Homeserver\Gerecht  $gerecht
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gerecht $gerecht)
+    {
+        //
+    }
+}
---
+++ database/factories/GerechtFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Homeserver\Gerecht::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
